1207b8c8791b900fb9898b663b670f417307d308
Danil Tutov <tutov.danil1234@gmail.com>
2024-11-11 20:42:24 +0300

    employee environment updated (lists added)

--- src/main/java/tech/reliab/course/tutovda/bank/service/EmployeeService.java
+++ src/main/java/tech/reliab/course/tutovda/bank/service/EmployeeService.java
@@ -3,6 +3,12 @@ package tech.reliab.course.tutovda.bank.service;
 import tech.reliab.course.tutovda.bank.entity.BankOffice;
 import tech.reliab.course.tutovda.bank.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee create(Employee employee);
+
+    Employee getEmployeeById(int id);
+
+    List<Employee> getAllEmployees();
 }
--- src/main/java/tech/reliab/course/tutovda/bank/service/impl/EmployeeServiceImpl.java
+++ src/main/java/tech/reliab/course/tutovda/bank/service/impl/EmployeeServiceImpl.java
@@ -1,9 +1,22 @@
 package tech.reliab.course.tutovda.bank.service.impl;
 
 import tech.reliab.course.tutovda.bank.entity.Employee;
+import tech.reliab.course.tutovda.bank.service.BankOfficeService;
 import tech.reliab.course.tutovda.bank.service.EmployeeService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EmployeeServiceImpl implements EmployeeService {
+    private final Map<Integer, Employee> employeesMap = new HashMap<>();
+    private final BankOfficeService bankOfficeService;
+
+    public EmployeeServiceImpl(BankOfficeService bankOfficeService) {
+        this.bankOfficeService = bankOfficeService;
+    }
+
     public Employee create(Employee employee) {
         if (employee == null) {
             return null;
@@ -14,6 +27,22 @@ public class EmployeeServiceImpl implements EmployeeService {
             return null;
         }
 
-        return new Employee(employee);
+        Employee newEmployee = new Employee(employee);
+        employeesMap.put(newEmployee.getId(), newEmployee);
+        bankOfficeService.addEmployee(newEmployee.getBankOffice().getId(), newEmployee);
+
+        return newEmployee;
+    }
+
+    public Employee getEmployeeById(int id) {
+        Employee employee = employeesMap.get(id);
+        if (employee == null) {
+            System.err.println("[ERROR] Employee with id ->" + id + " is not found");
+        }
+        return employee;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return new ArrayList<Employee>(employeesMap.values());
     }
 }
